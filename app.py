0aee780060d53dd9dc5c367c73b1f619ef278b56
hellovscholarpro-svg <hello.vscholarpro@gmail.com>
2026-04-21 16:52:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langchain_classic.chains import RetrievalQA
+from langchain.chains import RetrievalQA
 from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_chroma import Chroma
 from langchain_text_splitters import RecursiveCharacterTextSplitter
